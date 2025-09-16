f6118588d2a95ae55371be7eb19e2472d9adeada
CoraCote <cora.cote0903@gmail.com>
2025-09-16 06:33:42 -0400

    df

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],
+      domains: ['images.unsplash.com'],
     },
     async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-            },
-        ]
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${
+            process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+          }/api/:path*`,
+        },
+      ];
     },
-}
-
-module.exports = nextConfig
+  };
+  
+  module.exports = nextConfig;
+  
